a701814346d034e285797afaebddb73a7e564ae7
Kamlesh Bhatt <kb55262555@gmail.com>
2023-09-04 22:52:40 +0530

    Backend

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-
+const cors = require("cors");
 const { userRouter } = require("./routes/userRouter");
 const { connection } = require("./config/db");
 const { BlogRouter } = require("./routes/blogRouter");
